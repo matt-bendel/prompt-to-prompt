cc90e0e577e802002b9a0e11e1af76ea256674b4
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-16 19:51:27 -0400

    Moved notebook to script

--- null_text_w_ptp.py
+++ null_text_w_ptp.py
@@ -18,7 +18,7 @@ NUM_DDIM_STEPS = 50
 GUIDANCE_SCALE = 7.5
 MAX_NUM_WORDS = 77
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-ldm_stable = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=MY_TOKEN, scheduler=scheduler).to(device)
+ldm_stable = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=scheduler).to(device)
 try:
     ldm_stable.disable_xformers_memory_efficient_attention()
 except AttributeError:
