a8e8664c21928203772e373fe9ddcef46466269c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-20 11:17:27 -0400

    Moved notebook to script

--- ptp_utils.py
+++ ptp_utils.py
@@ -73,7 +73,7 @@ def diffusion_step(model, controller, latents, context, t, guidance_scale, low_r
     if nog:
         noise_pred = noise_pred_uncond
     else:
-        noise_pred = noise_pred_uncond + guidance_scale * (noise_prediction_text - noise_pred_uncond)
+        noise_pred = noise_prediction_text + guidance_scale * (noise_pred_uncond - noise_prediction_text)
 
     latents = model.scheduler.step(noise_pred, t, latents)["prev_sample"]
     latents = controller.step_callback(latents)
